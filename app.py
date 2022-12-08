0434fb482c7923bdffdf527975791648745d44ea
arberlisaj <arberlisaj1@gmail.com>
2022-12-08 21:13:23 +0100

    m

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def shumzim(x, y):
 def pjestim(x, y):
     return x / y
 
-# Printo opsionet aritmetike ne ekran
+
 print("Zgjidhni Operatorin Aritmetik:")
 print("1.Mbledhje")
 print("2.Zbritje")
@@ -19,10 +19,10 @@ print("3.Shumzim")
 print("4.Pjestim")
 
 while True:
-    # Useri fut opsionin e perzgjedhur
+   
     choice = input("Zgjidhni:(1/2/3/4): ")
 
-    # Bazuar tek inputi i user kryejm veprimet
+   
     if choice in ('1', '2', '3', '4'):
         num1 = float(input("Fusni Numrin e Pare: "))
         num2 = float(input("Fusni Numrin e Dyte: "))
@@ -39,7 +39,7 @@ while True:
         elif choice == '4':
             print(num1, "/", num2, "=", pjestim(num1, num2))
             
-       # Pyesim userin nese do te vazhdoj serish  
+            
         next_calculation = input("Deshironi te Vazhdoni ?  (po/jo): ")
         if next_calculation == "jo":
 
