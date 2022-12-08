c75cdda79b4347cca6132351167659ede9b1eeb7
arberlisaj <arberlisaj1@gmail.com>
2022-12-08 21:21:25 +0100

    m

--- app.py
+++ app.py
@@ -1,47 +1,47 @@
 
-def mbledhje(x, y):
+def add(x, y):
     return x + y
 
-def zbritje(x, y):
+def substract(x, y):
     return x - y
 
-def shumzim(x, y):
+def multiply(x, y):
     return x * y
 
-def pjestim(x, y):
+def devide(x, y):
     return x / y
 
 
-print("Zgjidhni Operatorin Aritmetik:")
-print("1.Mbledhje")
-print("2.Zbritje")
-print("3.Shumzim")
-print("4.Pjestim")
+print("Choose your operator:")
+print("1.addition")
+print("2.substraction")
+print("3.multiplication")
+print("4.division")
 
 while True:
    
-    choice = input("Zgjidhni:(1/2/3/4): ")
+    choice = input("Choose:(1/2/3/4): ")
 
    
     if choice in ('1', '2', '3', '4'):
-        num1 = float(input("Fusni Numrin e Pare: "))
-        num2 = float(input("Fusni Numrin e Dyte: "))
+        num1 = float(input("Insert the first Number: "))
+        num2 = float(input("Insert the second Number: "))
 
         if choice == '1':
-            print(num1, "+", num2, "=", mbledhje(num1, num2))
+            print(num1, "+", num2, "=", add(num1, num2))
 
         elif choice == '2':
-            print(num1, "-", num2, "=", zbritje(num1, num2))
+            print(num1, "-", num2, "=", substract(num1, num2))
 
         elif choice == '3':
-            print(num1, "*", num2, "=", shumzim(num1, num2))
+            print(num1, "*", num2, "=", multiply(num1, num2))
 
         elif choice == '4':
-            print(num1, "/", num2, "=", pjestim(num1, num2))
+            print(num1, "/", num2, "=", devide(num1, num2))
             
-            
-        next_calculation = input("Deshironi te Vazhdoni ?  (po/jo): ")
-        if next_calculation == "jo":
+
+        next_calculation = input("Do you want to continue?  (yes/no): ")
+        if next_calculation == "no":
 
           break
     
